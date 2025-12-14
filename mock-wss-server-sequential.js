e123c232634aae0c1a75d3567e3f43d9180c81e5
Vinayak Anvekar <fairvinay@gmail.com>
2025-12-15 03:45:16 +0530

    DEC 16 onwards expiries set , code at C:\n\artilleryfeed\screens

--- mock-wss-server-sequential.js
+++ mock-wss-server-sequential.js
@@ -275,10 +275,15 @@ const DELAY_BETWEEN_TRADES_MS = 30; // 30 mili seconds delay between individual 
 const CYCLE_INTERVAL_MS = 9000;     // The original 17 seconds cycle
 
   const symbols_const = [
-    { symbol: "NIFTY25093025300CE", id: "302418032",  },
+   /* { symbol: "NIFTY25093025300CE", id: "302418032",  },
     {symbol: "NIFTY25093025100PE",  id: "302418025"  },
     { symbol: "NIFTY25093025100CE",id: "302418024"   },
-    { symbol: "NIFTY25093025200PE", id: "302418029"  }
+    { symbol: "NIFTY25093025200PE", id: "302418029"  }*/
+     { symbol: "NIFTY25D1626000CE", id: "302418032",  },
+    {symbol: "NIFTY25D1626000PE",  id: "302418025"  },
+    { symbol: "NIFTY25D1626100CE",id: "302418024"   },
+ 	{ symbol: "NIFTY25D1626100PE", id: "302418029"  } 
+    
   ];
   // === 1. Send TrueData Real Time Data Service event immediately ===
   const mockEvent0 = {
@@ -486,25 +491,35 @@ function sendDelayedTrades(contracts, index = 0) {
  
   const trades_k_const = [
 
-        { id: "302418012", symbol: "NIFTY25O0724100CE", k: 180 },
-    { id: "302418013", symbol: "NIFTY25O0724100PE", k: 40 },
-    { id: "302418014", symbol: "NIFTY25O0724200PE", k: 360 },
-    { id: "302418015", symbol: "NIFTY25O0724200CE", k: 60 },
+        { id: "302418012", symbol: "NIFTY25D1624100CE", k: 180 },
+    { id: "302518013", symbol: "NIFTY25D1625100PE", k: 40 },
+    { id: "302418014", symbol: "NIFTY25D1625200PE", k: 360 },
+    { id: "302418015", symbol: "NIFTY25D1625200CE", k: 60 },
 
-    { id: "302418016", symbol: "NIFTY25O0724300CE", k: 180 },
-    { id: "302418017", symbol: "NIFTY25O0724300PE", k: 40 },
-    { id: "302418018", symbol: "NIFTY25O0724400PE", k: 360 },
-    { id: "302418019", symbol: "NIFTY25O0724400CE", k: 60 },
+    { id: "302418016", symbol: "NIFTY25D1625300CE", k: 180 },
+    { id: "302418017", symbol: "NIFTY25D1625300PE", k: 40 },
+    { id: "302418018", symbol: "NIFTY25D1625400PE", k: 360 },
+    { id: "302418019", symbol: "NIFTY25D1625400CE", k: 60 },
 
-     { id: "302418020", symbol: "NIFTY25O0724500CE", k: 180 },
-    { id: "302418021", symbol: "NIFTY25O0724500PE", k: 40 },
-    { id: "302418022", symbol: "NIFTY25O0724600PE", k: 360 },
-    { id: "302418023", symbol: "NIFTY25O0724600CE", k: 60 },
+     { id: "302418020", symbol: "NIFTY25D1625500CE", k: 180 },
+    { id: "302418021", symbol: "NIFTY25D1625500PE", k: 40 },
+    { id: "302418022", symbol: "NIFTY25D1625600PE", k: 360 },
+    { id: "302518023", symbol: "NIFTY25D1625600CE", k: 60 },
 
-    { id: "302418032", symbol: "NIFTY25O0724700CE", k: 180 },
-    { id: "302418025", symbol: "NIFTY25O0724700PE", k: 40 },
-    { id: "302418024", symbol: "NIFTY25O0724800PE", k: 360 },
-    { id: "302418029", symbol: "NIFTY25O0724800CE", k: 60 }
+    { id: "302418032", symbol: "NIFTY25D1625700CE", k: 180 },
+    { id: "302418025", symbol: "NIFTY25D1625700PE", k: 40 },
+    { id: "302418024", symbol: "NIFTY25D1625800PE", k: 360 },
+    { id: "302418029", symbol: "NIFTY25D1625800CE", k: 60 },
+    	
+        { id: "302419032", symbol: "NIFTY25D1625900CE", k: 180 },
+    { id: "302419025", symbol: "NIFTY25D1625900PE", k: 40 },
+    { id: "302419024", symbol: "NIFTY25D1626000PE", k: 360 },
+    { id: "302419029", symbol: "NIFTY25D1626000CE", k: 60 },
+    			
+    	 { id: "302420032", symbol: "NIFTY25D1626050CE", k: 180 },
+    { id: "302420025", symbol: "NIFTY25D1626050PE", k: 40 },
+    { id: "302420024", symbol: "NIFTY25D1626100PE", k: 360 },
+     { id: "302420029", symbol: "NIFTY25D1626100CE", k: 60 }
   ];
  
 
